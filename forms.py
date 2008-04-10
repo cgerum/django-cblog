9e0b029f739e42076062350c949984d3ded4247d
christoph@gerum.de <christoph@gerum.de@3f52f1a6-644a-0410-bfe5-fd9c3412c68c>
2008-04-10 04:30:26 +0000

    Just a bit of refactoring.
    git-svn-id: http://django-cblog.googlecode.com/svn/trunk@16 3f52f1a6-644a-0410-bfe5-fd9c3412c68c

--- forms.py
+++ forms.py
@@ -1,7 +1,25 @@
 from django import newforms as forms
-from cblog.models import Blogpost
+from cblog.models import Blogpost 
+from django.newforms.util import smart_unicode
+from django.newforms.widgets import flatatt
+from django.utils.safestring import mark_safe
+
+try:
+    from photologue.models import Photo
+    photologue = True
+except ImportError:
+    photologue=false
+    
+
+#class PostEditField(forms.widgets.Textarea):
+#    def render(self, name, value, attrs=None):
+#        value = smart_unicode(value)
+#        attrs = self.build_attrs(attrs, name=name)
+#        
+#        return mark_safe(u'<textarea %s>%s</textarea>' % (flatatt(attrs), value))
 
 class EditPostForm(forms.ModelForm):
+    #post = forms.fields.CharField(widget=PostEditField())
     class Meta:
         model = Blogpost
         exclude = ('slug',)
